7b1a75127d7655122b7362daf1694920e472ebf7
Tanzil-khan <155762084+Tanzil4@users.noreply.github.com>
2024-04-20 01:13:19 +0500

    Add files via upload

---
+++ arry ka square/multiplay.js
@@ -0,0 +1,19 @@
+let matrix1 = [
+    [4, 6, 7],
+    [8, 3, 5],
+    [0, 2, 8],
+]
+let matrix2 = [
+    [5, 4, 6],
+    [7, 2, 5],
+    [0, 1, 9],
+]
+
+
+for (let t = 0; t < 3; t++) {
+    
+    for (let a = 0; a < 3; a++) {
+       console.log(matrix1[t][a] * matrix2[a][t]);
+        
+    }
+}
